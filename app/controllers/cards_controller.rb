882ff8da0aeed7401e9d4bb5d724df74dc84b832
Fil <vtl.filimonov@gmail.com>
2017-07-12 16:02:15 +0300

    Add New action and view

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,4 +2,12 @@ class CardsController < ApplicationController
 	def index
     @cards = Card.all
   end
+
+  def show
+  	@card = Card.find(params[:id])
+  end
+
+  def new
+  	@card = Card.new
+  end
 end
