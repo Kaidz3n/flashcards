d788b607b10f2cab1d297e6b402c4b9d56c9dbdd
Fil <vtl.filimonov@gmail.com>
2017-07-12 16:24:21 +0300

    Add /view/show and action create

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -10,4 +10,19 @@ class CardsController < ApplicationController
   def new
   	@card = Card.new
   end
+
+  def create
+  	@card = Card.new(card_params)
+  	if @card.save
+			redirect_to @card
+		else
+			render :new
+		end
+	end
+
+	private
+
+	def card_params
+		params.require(:card).permit(:original_text, :translated_text, :review_date)
+	end
 end
