cb4a5540d78e0933818776b9eda77396f1ab7e31
Fil <vtl.filimonov@gmail.com>
2017-07-12 16:40:26 +0300

    Add actions edit,update

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -20,6 +20,19 @@ class CardsController < ApplicationController
 		end
 	end
 
+	def edit
+		@card = Card.find(params[:id])
+	end
+
+	def update
+		@card = Card.find(params[:id])
+		if @card.update_attributes(card_params)
+			redirect_to @card
+		else
+			render :edit
+		end
+	end
+
 	private
 
 	def card_params
