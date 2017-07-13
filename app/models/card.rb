f975ad6373c4b65609ef10f5cbb724b1ccbe3948
Fil <vtl.filimonov@gmail.com>
2017-07-13 10:35:12 +0300

    Update validation, _form view

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,9 @@
 class Card < ApplicationRecord
-	validates_presence_of :original_text, :translated_text 
+	validates_presence_of :original_text, :translated_text, :review_date
+	validates_uniqueness_of :original_text, :translated_text
+
+	validates :original_text, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only allows letters" }
+  validates :original_text, format: { with: /\А[а-яА-Я]+\я/,
+    message: "только кириллица" }
 end
